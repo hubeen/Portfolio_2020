44eb50768a98e972857534f9956b81eff9fad0ff
hubeen <hubeen__@naver.com>
2020-03-28 15:48:29 +0900

    modify: home files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,23 @@
 import React from "react";
 import "./Home.scss";
 const Home = ({ ...props }) => {
-    const handleClick = () => {
-        props.history.push("/about");
-    };
-
-    return (
-        <main className="main home nes-container is-dark is-rounded" onClick={handleClick}>
-            <div className="title">
-                <h1 className="title-text title-text--primary">Seunghyun Moon</h1> 
-                <h1 className="title-text title-text--primary">Alfred Hubeen</h1>
-                <h2 className="title-text title-text--secondary">Freelancer</h2>
-                <h2 className="title-text title-text--tertiary nes-pointer flash">
-                    - Click to Start -
-                </h2>
-            </div>
-        </main>
-    );
-
+  const handleClick = () => {
+    props.history.push("/about");
+  };
+  return (
+    <main
+      className="main home nes-container is-dark is-rounded"
+      onClick={handleClick}
+    >
+      <div className="title">
+        <h1 className="title-text title-text--primary">Seunghyun Moon</h1>
+        <h2 className="title-text title-text--secondary">Freelance</h2>
+        <h2 className="title-text title-text--tertiary nes-pointer flash">
+          - Click to Start -
+        </h2>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
