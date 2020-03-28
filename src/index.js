89e6294d7c56dfaf221527901c4cd1ed9c1e32d2
hubeen <hubeen__@naver.com>
2020-03-28 15:47:52 +0900

    modify: index files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import "nes.css/css/nes.css";
-
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
-    document.getElementById("root")
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
 );
